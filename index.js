fc275fdaa7fb030da46acbb8e58108bd1d1fdfb9
annerachelpinto <annerachelpinto@gmail.com>
2021-10-03 12:50:48 +0530

    Save random image using Promise

--- index.js
+++ index.js
@@ -17,16 +17,42 @@ function randomString(length) {
 }
 
 
-//Callbacks
-superagent.get(`https://robohash.org/${randomString(6)}`)
-    .end( (err, res) => {
-        console.log(res.request.url);
-        fs.writeFile("./randomImage.txt", res.request.url, (err) => {
+//Promises
+function writeFilePromise(fileLocation, result) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileLocation, result, (err) => {
             if (err) {
-                console.log("Not able to write image in txt file", err);
-                return;
+                reject('not able to write to the file')
             }
-            console.log(`Random Image successfully written inside the file`);
-        });
-    });
+            resolve()
+        })
+    })
+}
+superagent.get(`https://robohash.org/${randomString(6)}`)
+    .end((err, res) => {
+        console.log(res.request.url);
+        return writeFilePromise('./randomImage.txt', res.request.url)
+    .then(() => {
+        console.log('Sucessfully written the file')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+
+
+
+//Callbacks
+// superagent.get(`https://robohash.org/${randomString(6)}`)
+//     .end((err, res) => {
+//         console.log(res.request.url);
+//         fs.writeFile("./randomImage.txt", res.request.url, (err) => {
+//             if (err) {
+//                 console.log("Not able to write image in txt file", err);
+//                 return;
+//             }
+//             console.log(`Random Image successfully written inside the file`);
+//         });
+//     });
 
